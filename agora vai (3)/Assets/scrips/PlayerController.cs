79f7862d539dead11d7e630fd6573250ba271b6c
vitorhermeto <54818607+vitorhermeto@users.noreply.github.com>
2019-09-03 13:39:36 -0300

    add empty project

---
+++ agora vai (3)/Assets/scrips/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player1 : MonoBehaviour
+{
+    public float pulo;
+    Rigidboby2D rd;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+      if (Input.GetKeyDown(KeyCode.Space))
+      {
+          rb.AddForce(new Vector2(0,pulo));
+      }  
+    }
+}
